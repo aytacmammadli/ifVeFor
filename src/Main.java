5fa41364c2178744069504df6303ba5f27dd2902
Aytac <aytacmammadli6@gmail.com>
2023-04-25 20:23:18 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int eded1 = 1;
+        int eded2 = 10;
+        if (eded1 < eded2) {
+            System.out.println("eded1 eded2-den kicikdir " + eded1);
+        } else if (eded1 > eded2) {
+            System.out.println("eded1 eded2-den böyükdür " + eded1);
+        } else {
+            System.out.println("iki eded bir-birine beraberdir");
+        }
+
+    }
+}
